860a8bb3c13ebacfa89f0779e8f7526addc17e95
RoCooEngi <astrodublewaffle@gmail.com>
2025-02-24 23:57:09 +0400

    Initial commit
    Created common page template and main page

---
+++ skyward/main/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Функция для отображения главной страницы
+def home(request):
+    # Рендерим шаблон index.html и возвращаем его как ответ на запрос
+    return render(request, 'index.html')
