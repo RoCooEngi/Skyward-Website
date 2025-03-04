908394e83e9e566a1477cb0d6a4cddf88412174a
RoCooEngi <astrodublewaffle@gmail.com>
2025-03-04 23:07:51 +0400

    Main page mofified     added custom fonts, remade intro, added services and improved header dimensions

--- skyward/main/static/js/responsive.js
+++ skyward/main/static/js/responsive.js
@@ -1,12 +1,12 @@
 // Функция для подгонки ширины контента
 function adjustContentWidth() {
-    const contentContainers = document.querySelectorAll('.content-container, .symbols-container, .footer-content');
+    const contentContainers = document.querySelectorAll('.footer-content, .nav, .content-container, .symbols-container');
     const viewportWidth = window.innerWidth;
 
     contentContainers.forEach(container => {
         // Устанавливаем ширину контейнера равной ширине viewport, если она меньше 1300px
         if (viewportWidth < 1300) {
-            container.style.width = `${viewportWidth-40}px`;
+            container.style.width = `${viewportWidth - 0.1 * viewportWidth}px`;
         } else {
             container.style.width = '1300px'; // Фиксируем ширину на 1300px
         }
